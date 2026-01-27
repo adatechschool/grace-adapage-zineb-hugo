523a176b5a5d27e8c7ca33b122ef9423deedf2fb
zineb712 <zinebdouich312@gmail.com>
2026-01-27 12:02:23 +0100

    Modif Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,36 @@
-import Navbar from "../components/Navbar";
+import type { GraceProps} from "../App";
 
-export default function Home() {
+type HomeProps = Pick<GraceProps, "name" | "description" | "img">;
+;
+
+export default function Home({ name, description, img }: HomeProps) {
   return (
-    <>
-      <Navbar />
-      <main className="pt-20">
-        <h1 className="text-3xl text-center">Page Home</h1>
-      </main>
-    </>
+    <main className="min-h-screen pt-24 px-6 bg-gray-50">
+      
+      // 
+      <section className="max-w-6xl mx-auto grid md:grid-cols-2 gap-12 items-center">
+        
+        // Description 
+        <div>
+          <h1 className="text-4xl font-bold text-gray-900 mb-6">
+            {name}
+          </h1>
+
+          <p className="text-lg text-gray-700 leading-relaxed">
+            {description}
+          </p>
+        </div>
+
+        // Image 
+        <div className="flex justify-center">
+          <img
+            src={img}
+            alt={name}
+            className="rounded-xl shadow-xl max-h-[450px] object-cover"
+          />
+        </div>
+
+      </section>
+    </main>
   );
 }
