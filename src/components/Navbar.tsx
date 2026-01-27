e63db096c354bc4c6c29a134223c842f132b8b8e
Hgo92 <hugo_monier@yahoo.fr>
2026-01-27 12:10:32 +0100

    retiré AdaPage de NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,6 @@ export default function Navbar({logo} : any) {
     <nav className="fixed top-0 w-full bg-white shadow flex items-center justify-between px-6 py-3">
       <div className="flex items-center gap-2">
         <img src={logo} alt="Logo du projet" className="h-8 w-8" />
-        <span className="font-bold text-lg">AdaPage</span>
       </div>
 
       <ul className="flex gap-6">
