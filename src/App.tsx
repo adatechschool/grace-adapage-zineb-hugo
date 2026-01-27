cb7a19acf30a150c5a54140d92a4b79bc25800bb
Hgo92 <hugo_monier@yahoo.fr>
2026-01-27 12:08:48 +0100

    Correction logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar"; 
 import Quote from "./pages/Quotes";
+import grace from "./assets/grace.svg"
 
   export interface GraceProps {
     name : string
@@ -28,7 +29,7 @@ name : "Grace Murray Hopper",
 description : "Grace Murray Hopper est une informaticienne, mathématicienne et Rear admiral (lower half) de la marine américaine, née le 9 décembre 1906 à New York et morte le 1er janvier 1992 dans le comté d'Arlington. Elle est la conceptrice du premier compilateur en 1951 (A-0 System) et du langage Cobol en 1959. En 1943, Grace Hopper s'engage dans la marine américaine. Promue au grade de lieutenante, elle est affectée l'année suivante au Bureau of Ordnance Computation Project de l'université Harvard. Elle travaille dans l'équipe de Howard Aiken sur l'ordinateur Harvard Mark I. Elle fait partie du premier groupe, comptant trois personnes, apprenant à le programmer. À la fin de la Seconde Guerre mondiale, elle quitte le service actif de la marine, mais continue à travailler au développement des ordinateurs Harvard Mark II puis Harvard Mark III pour le compte du Computation Laboratory d'Harvard. Hopper conçoit en 1951 le premier compilateur pour UNIVAC I, nommé A-0 System. En 1955, elle invente le langage FLOW-MATIC aussi appelé B-0. À partir de 1957, elle travaille pour IBM, où elle défend l'idée qu'un programme devrait pouvoir être écrit dans un langage proche de l'anglais plutôt que d'être calqué sur le langage machine, comme l'assembleur. De cette idée naît le langage Cobol en 1959.",
 country : "Etats-Unis" ,
 img : "https://upload.wikimedia.org/wikipedia/commons/3/37/Grace_Hopper_and_UNIVAC.jpg",
-logo :"./assets/grace.svg", 
+logo : grace, 
 quotes : [
 {quote : "A partir de maintenant, quand quelque chose ne fonctionne pas dans un ordinateur, on parlera de bug (insecte).", date: "16 avril 1984"},
 {quote : "J'ai toujours été plus intéressée par le futur que par le passé.", date : "Octobre 1994"}, 
@@ -44,7 +45,7 @@ dod : "1er janvier 1992"
   return (
 
     <>
-    <Navbar />
+    <Navbar logo = {graceData.logo} />
     <Routes>
    <Route path="/" element={<Home name={graceData.name} description={graceData.description} img={graceData.img} />} />
    <Route path="/citations" element= {<Quote quotes = {graceData.quotes}/>} />     
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
-export default function Navbar() {
+
+
+export default function Navbar({logo} : any) {
   return (
     <nav className="fixed top-0 w-full bg-white shadow flex items-center justify-between px-6 py-3">
       <div className="flex items-center gap-2">
-        <img src="/logo.png" alt="Logo du projet" className="h-8 w-8" />
+        <img src={logo} alt="Logo du projet" className="h-8 w-8" />
         <span className="font-bold text-lg">AdaPage</span>
       </div>
 
