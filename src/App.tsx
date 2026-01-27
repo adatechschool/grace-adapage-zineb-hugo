b085a9b29b81b58ea45c2299b27a3b0dd8b398fb
zineb712 <zinebdouich312@gmail.com>
2026-01-27 11:22:02 +0100

    nouveau import dans app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar"; 
 
 let graceData = {
 name : "Grace Murray Hopper",
@@ -22,7 +23,7 @@ dod : "1er janvier 1992"
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+   <Route path="/" element={<Home data={graceData} />} />    
     </Routes>
   );
 }
