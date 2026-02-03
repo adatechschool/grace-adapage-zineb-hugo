290ba43697178abf700b94854b7ba617c7da573c
Hgo92 <hugo_monier@yahoo.fr>
2026-02-03 11:28:52 +0100

    modif mise en page code

--- src/pages/Frise.tsx
+++ src/pages/Frise.tsx
@@ -6,7 +6,6 @@ interface friseProps {
 }
 
 export default function Frise({graceData} : friseProps) {
-    
     return (
         <main className="min-h-screen pt-32
                  bg-gradient-to-br from-indigo-100 via-pink-100 to-yellow-100
@@ -15,14 +14,14 @@ export default function Frise({graceData} : friseProps) {
 
         
             <div className="absolute inset-0 overflow-hidden">
-            <div className="absolute w-96 h-96 bg-purple-300 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob top-10 left-20"></div>
-            <div className="absolute w-96 h-96 bg-pink-300 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob animation-delay-2000 top-64 left-72"></div>
-            <div className="absolute w-96 h-96 bg-yellow-300 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob animation-delay-4000 top-32 left-1/2"></div>
-        
+                <div className="absolute w-96 h-96 bg-purple-300 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob top-10 left-20"></div>
+                <div className="absolute w-96 h-96 bg-pink-300 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob animation-delay-2000 top-64 left-72"></div>
+                <div className="absolute w-96 h-96 bg-yellow-300 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blob animation-delay-4000 top-32 left-1/2">
+            </div>
     
       </div>
-            <h1 className="text-4xl font-extrabold text-gray-900 leading-tight  dark:text-white tracking-wide text-center">Grace Hopper, une vie dédiée au progrès informatique</h1>
-            <div className="flex flex-col relative my-5 after:content-[''] after:bg-[#e17b77] after:absolute after:left-[calc(50%-2px)] after:w-1 after:h-full ">
+        <h1 className="text-4xl font-extrabold text-gray-900 leading-tight  dark:text-white tracking-wide text-center">Grace Hopper, une vie dédiée au progrès informatique</h1>
+        <div className="flex flex-col relative my-5 after:content-[''] after:bg-[#e17b77] after:absolute after:left-[calc(50%-2px)] after:w-1 after:h-full ">
                 
         {graceData.timeline.map((data, index) => {
             return <TimelineItem data={data} index={index} key={index}/>
